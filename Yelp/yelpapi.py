2420e8fd5ffbf057ee719e02b3e4475ac41fbe82
cywu1031 <cywu1031@gmail.com>
2016-11-03 20:48:50 -0700

    fix errors

--- Yelp/yelpapi.py
+++ Yelp/yelpapi.py
@@ -8,6 +8,25 @@ import json
 queryTime = 5
 kafkaTopic = 'yelp'
 
+def CalculateAvgRating(s):
+    params = {
+        'category_filter': s
+    }
+    i = 0
+    average_rating = 0
+    total_rating = 0
+    response = client.search('San Francisco', params)
+    
+    availableDataLen = len(response.businesses)
+
+    while( i < availableDataLen):
+        total_rating = total_rating + int(response.businesses[i].rating)
+        i = i+1
+
+    average_rating = total_rating / availableDataLen
+    
+    return str(average_rating)
+
 with io.open('yelpkey.json') as cred: 
     creds = json.load(cred)
     auth = Oauth1Authenticator(
@@ -19,23 +38,11 @@ with io.open('yelpkey.json') as cred:
     
     client = Client(auth)
     producer = KafkaProducer(bootstrap_servers = 'localhost:9092')
-    while(ture):
+    while(True):
        producer.send(kafkaTopic, CalculateAvgRating('chinese').encode('utf-8'))
        producer.send(kafkaTopic, CalculateAvgRating('Indian').encode('utf-8'))
        producer.send(kafkaTopic, CalculateAvgRating('mediterranean').encode('utf-8'))
        time.sleep(queryTime)  
 
-def CalculateAvgRating(s):
-    params = {
-        'category_filter': s
-    }
-    i = 0
-    average_rating = 0
-    total_rating = 0
-    response = client.search('San Francisco', **params)
-    while( i < int(response.total) ):
-        total_rating = total_rating + int(response.businesses[i]. rating)
-        i = i+1
-    average_rating = total_rating/int(response.total)
-    return average_rating
+
     
